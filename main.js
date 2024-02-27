13797652ff2248f8c55f26643134d154e8bb8f7e
Hua Kieu Lam <157774509+Hua-Kieu-Lam@users.noreply.github.com>
2024-02-27 18:30:29 +0700

    clear content main.js

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-var button = document.querySelector('h1');
-var isRed = false;
 
-function changeColor() {
-    if (isRed) {
-        button.classList.remove('red-color');
-        isRed = false;
-    } else {
-        button.classList.add('red-color');
-        isRed = true;
-    }
-}
