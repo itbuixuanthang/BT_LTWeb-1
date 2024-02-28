ad152b8c2e4f826b9598fb0cb774a14d3844a392
Lam Kiều <huakieulam@gmail.com>
2024-02-28 17:35:09 +0700

    .

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-
-const posts = document.querySelectorAll('.post');
-
-const postContentContainer = document.getElementById('showPost');
-
-posts.forEach(post => {
-    post.addEventListener('click', () => {
-        posts.forEach(p => {
-            if (p !== post) {
-                p.style.display = 'none';
-            }
-        });
-
-        postContentContainer.innerHTML = '';
-
-        const content = post.innerHTML;
-
-        const contentElement = document.createElement('div');
-        contentElement.innerHTML = content;
-
-        postContentContainer.appendChild(contentElement);
-    });
-});
+function openDetailsPage(pageURL) {
+    window.location.href = pageURL;
+}
